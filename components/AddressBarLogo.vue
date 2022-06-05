02ea204b1affe38c9ae1bfc720821d488bdb1b4c
Taurin190 <taura.koichi@gmail.com>
2022-06-05 09:49:04 +0900

    delete default navigation items

---
+++ components/AddressBarLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <img
+    class="address-bar-logo"
+    alt="Sample CINEMAS"
+    src="/address-bar-logo.png"
+  />
+</template>
+
+<style>
+.address-bar-logo {
+  height: 45px;
+  width: 210px;
+}
+</style>

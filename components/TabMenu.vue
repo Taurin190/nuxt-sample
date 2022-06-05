75f6e534e7d918eac4d39dc91fd0a9a3cd9c4b8b
Taurin190 <taura.koichi@gmail.com>
2022-06-05 15:37:09 +0900

    modify to delete unnecessary parts

--- components/TabMenu.vue
+++ components/TabMenu.vue
@@ -1,28 +1,10 @@
 <template>
   <v-card>
     <v-tabs centered icons-and-text>
-      <!-- <v-tabs-slider></v-tabs-slider> -->
       <v-tab v-for="tab in tabs" :key="tab.index" :href="tab.to">
         {{ tab.title }}
         <v-icon>{{ tab.icon }}</v-icon>
       </v-tab>
-
-      <!-- <v-tab href="#tab-2">
-        Favorites
-        <v-icon>mdi-heart</v-icon>
-      </v-tab>
-
-      <v-tab href="#tab-3">
-        Nearby
-        <v-icon>mdi-account-box</v-icon>
-      </v-tab> -->
-      <v-tabs-items v-model="tabs">
-        <v-card flat>
-          <v-card-text>
-            {{ title }}
-          </v-card-text>
-        </v-card>
-      </v-tabs-items>
     </v-tabs>
   </v-card>
 </template>

0ed42e49d06725475777c16ca6a205e0259582c2
Taurin190 <taura.koichi@gmail.com>
2022-06-08 07:09:15 +0900

    add image banner list

---
+++ components/ImageBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-col cols="4">
+    <a>
+      <img src="/building_cinema_shitsunai.png" />
+    </a>
+  </v-col>
+</template>
+<style scoped>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
+<script>
+export default {
+  name: "ImageBanner",
+};
+</script>
---
+++ components/ImageBanners.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row align="align">
+      <ImageBanner />
+      <ImageBanner />
+      <ImageBanner />
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ImageBanners",
+};
+</script>

15e77901c45e0481905c44a3ebe542b55f193e3a
Taurin190 <taura.koichi@gmail.com>
2022-06-24 03:07:18 +0900

    add campaign list

---
+++ components/molecules/CampaignLink.vue
@@ -0,0 +1,39 @@
+<template>
+  <nuxt-link
+    :to="to"
+    class="center block"
+  >
+    <v-row align="left" dense>
+      <v-col cols="6">
+        <v-img :src="image_path" />
+      </v-col>
+      <v-col cols="6">
+        <span>
+          {{ description }}
+        </span>
+      </v-col>
+    </v-row>
+  </nuxt-link>
+</template>
+
+<style>
+</style>
+<script>
+export default {
+  name: "CampaignLink",
+  props: {
+    to: {
+      type: String,
+      default: "",
+    },
+    image_path: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
--- components/organisms/CampaignListPanel.vue
+++ components/organisms/CampaignListPanel.vue
@@ -10,7 +10,15 @@
         />
       </v-col>
     </v-row>
-    <v-row justify="left" align="center"> </v-row>
+    <v-row justify="left" align="center">
+      <v-col v-for="campaign in campaign_list" :key="campaign.index" cols="12">
+        <campaign-link
+          :to="campaign.to"
+          :description="campaign.description"
+          :image_path="campaign.image_path"
+        />
+      </v-col>
+    </v-row>
   </div>
 </template>
 <style scoped>
@@ -19,9 +27,10 @@ div.campaign-list-panel {
 }
 </style>
 <script>
+import CampaignLink from "../molecules/CampaignLink.vue";
 import PanelTitle from "../molecules/PanelTitle.vue";
 export default {
-  components: { PanelTitle },
+  components: { PanelTitle, CampaignLink },
   name: "CampaignListPanel",
   data() {
     return {
@@ -31,6 +40,38 @@ export default {
         title: "キャンペーン",
         etitle: "CAMPAIGNS",
       },
+      campaign_list: [
+        {
+          index: 1,
+          to: "/events/movie1",
+          image_path: "/movie_tokusatsu_kaiju.png",
+          description: "『劇場版　太郎と次郎』抽選で豪華賞品が当たる",
+        },
+        {
+          index: 2,
+          to: "/events/movie1",
+          image_path: "/movie_tokusatsu_kaiju.png",
+          description: "『劇場版　太郎と次郎』抽選で豪華賞品が当たる",
+        },
+        {
+          index: 3,
+          to: "/events/movie1",
+          image_path: "/movie_tokusatsu_kaiju.png",
+          description: "『劇場版　太郎と次郎』抽選で豪華賞品が当たる",
+        },
+        {
+          index: 4,
+          to: "/events/movie1",
+          image_path: "/movie_tokusatsu_kaiju.png",
+          description: "『劇場版　太郎と次郎』抽選で豪華賞品が当たる",
+        },
+        {
+          index: 5,
+          to: "/events/movie1",
+          image_path: "/movie_tokusatsu_kaiju.png",
+          description: "『劇場版　太郎と次郎』抽選で豪華賞品が当たる",
+        },
+      ],
     };
   },
 };

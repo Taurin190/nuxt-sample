f96fa428a9b4bd67da6029bb7dd7afd7b0042e34
Taurin190 <taura.koichi@gmail.com>
2022-07-09 11:45:09 +0900

    set movie card style

--- components/molecules/MovieCard.vue
+++ components/molecules/MovieCard.vue
@@ -1,10 +1,21 @@
 <template>
-  <nuxt-link :to="to" class="center block">
-    <v-img :src="image_path" />
+  <nuxt-link :to="to" class="center block text-decoration-none">
+    <v-img class="mb-1" :src="image_path" />
     <span>{{ title }}</span>
   </nuxt-link>
 </template>
-
+<style scoped>
+a {
+  color: black;
+}
+a:hover {
+  color: #4da7d8;
+}
+div.v-image:hover {
+  opacity: 0.8;
+  color: white;
+}
+</style>
 <script>
 export default {
   name: "MovieCard",

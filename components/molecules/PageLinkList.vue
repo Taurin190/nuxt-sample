888087d5a8ba5d7ca5224b128c3845f2e56b5d36
Taurin190 <taura.koichi@gmail.com>
2022-07-23 13:31:44 +0900

    modify footer link

--- components/molecules/PageLinkList.vue
+++ components/molecules/PageLinkList.vue
@@ -1,16 +1,44 @@
 <template>
-  <v-col cols="4">
-    <dl>
-      <dd>
-        <ul>
-          <li v-for="link in links" :key="link.name">
-            <nuxt-link :to="link.to" class="footer-link">
-              {{ link.name }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </dd>
-    </dl>
+  <v-col cols="12" class="d-flex footer-link">
+    <v-col cols="4">
+      <dl>
+        <dd>
+          <ul>
+            <li v-for="link in page_links" :key="link.name">
+              <nuxt-link :to="link.to" class="footer-link">
+                {{ link.name }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </dd>
+      </dl>
+    </v-col>
+    <v-col cols="4">
+      <dl>
+        <dd>
+          <ul>
+            <li v-for="link in inquiry_links" :key="link.name">
+              <nuxt-link :to="link.to" class="footer-link">
+                {{ link.name }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </dd>
+      </dl>
+    </v-col>
+    <v-col cols="4">
+      <dl>
+        <dd>
+          <ul>
+            <li v-for="link in corporate_links" :key="link.name">
+              <nuxt-link :to="link.to" class="footer-link">
+                {{ link.name }}
+              </nuxt-link>
+            </li>
+          </ul>
+        </dd>
+      </dl>
+    </v-col>
   </v-col>
 </template>
 <style scoped>
@@ -22,12 +50,15 @@ dd {
 li > a.footer-link {
   color: black;
 }
+div.footer-link {
+  padding: 0;
+}
 </style>
 <script>
 export default {
   name: "PageLinkList",
   props: {
-    links: {
+    page_links: {
       type: Array,
       default: [
         {
@@ -49,6 +80,32 @@ export default {
         {
           name: "チケット購入・照会",
           to: "/tickets",
+        },
+      ],
+    },
+    corporate_links: {
+      type: Array,
+      default: [
+        {
+          name: "お問い合わせ",
+          to: "/nowshowing",
+        },
+        {
+          name: "よくあるご質問",
+          to: "/comingsoon",
+        },
+      ],
+    },
+    inquiry_links: {
+      type: Array,
+      default: [
+        {
+          name: "採用情報",
+          to: "/nowshowing",
+        },
+        {
+          name: "109シネマズ経営理念",
+          to: "/comingsoon",
         },
       ],
     },

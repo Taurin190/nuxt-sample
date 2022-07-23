b3c7f183ac3cacbf6dc34a5286f4df670eddccc2
Taurin190 <taura.koichi@gmail.com>
2022-07-23 14:18:13 +0900

    improve imax link

--- components/molecules/IMaxLink.vue
+++ components/molecules/IMaxLink.vue
@@ -1,18 +1,32 @@
 <template>
   <v-col cols="5">
-    <nuxt-link class="black--text px-1 text-decoration-none" to="/">HOME</nuxt-link>
-    <nuxt-link class="black--text px-1 text-decoration-none left-divider" to="/imax">IMAX</nuxt-link>
-    <nuxt-link class="black--text px-1 text-decoration-none left-divider" to="/4dx">4DX/ScreenX/4DX Screen</nuxt-link>
+    <nuxt-link 
+    v-for="link in links"
+    :key="link.index"
+    v-bind:class="[link.index > 1 ? 'left-divider': '']"
+    class="black--text px-2 my-2 text-decoration-none" 
+    :to="link.to"
+      >{{ link.text }}</nuxt-link
+    >
   </v-col>
 </template>
 <style scoped>
 a.left-divider {
-      border-left: 1px solid black;
+  border-left: 1px solid black;
 }
-
 </style>
 <script>
 export default {
-  name: "ImageBanner",
+  name: "IMaxLink",
+  props: {
+    links: {
+      type: Array,
+      default: [
+        { text: "HOME", to: "/", index: 1 },
+        { text: "IMAX", to: "/imax", index: 2 },
+        { text: "4DX/ScreenX/4DX Screen", to: "/4dx", index: 3 },
+      ],
+    },
+  },
 };
 </script>

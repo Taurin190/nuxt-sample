bf5c2af33ddee8c52bb7c9ac46740922d0d09900
Taurin190 <taura.koichi@gmail.com>
2022-09-03 08:55:56 +0900

    add event page

---
+++ components/organisms/EventListPanel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="campaign-list-panel py-5 mb-5 px-4 rounded">
+    <v-row justify="left" align="center">
+      <v-col cols="12">
+        <panel-title
+          :to="panel_title.to"
+          :icon="panel_title.icon"
+          :title="panel_title.title"
+          :etitle="panel_title.etitle"
+        />
+      </v-col>
+    </v-row>
+    <v-row justify="left" align="center">
+      <v-col v-for="event in events" :key="event.id" cols="12">
+        <campaign-link
+          :to="event.link"
+          :description="event.description"
+          :image_path="event.image_path"
+        />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<style scoped>
+div.campaign-list-panel {
+  background-color: white;
+}
+</style>
+<script>
+import { useContext, useAsync } from "@nuxtjs/composition-api";
+import CampaignLink from "../molecules/CampaignLink.vue";
+import PanelTitle from "../molecules/PanelTitle.vue";
+export default {
+  components: { PanelTitle, CampaignLink },
+  name: "EventListPanel",
+  data() {
+    return {
+      panel_title: {
+        to: "/events",
+        icon: "mdi-calendar-check",
+        title: "イベント",
+        etitle: "EVENTS",
+      },
+    };
+  },
+  setup() {
+    const { app } = useContext()
+    const events = useAsync(async () => {
+      const response = await app.$repositories('event').get()
+      console.log(response);
+      return response.data
+    })
+
+    return { events };
+  }
+};
+</script>
--- pages/events.vue
+++ pages/events.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <tab-menu :active=4 />
+    <tab-menu :active="4" />
+    <cinema-page-title
+      title="イベント/キャンペーン一覧"
+      sub_title="EVENTS/CAMPAIGNS"
+    />
     <v-row>
-      <v-col class="text-center">
-        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-        <blockquote class="blockquote">
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+      <v-col cols="8">
+        <event-list-panel />
+      </v-col>
+      <v-col cols="4">
+        <theater-panel />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="8">
+        <campaign-list-panel />
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
-import TabMenu from '~/components/organisms/TabMenu.vue';
+import TabMenu from "~/components/organisms/TabMenu.vue";
+import CinemaPageTitle from "~/components/atoms/CinemaPageTitle.vue";
+import CampaignListPanel from "~/components/organisms/CampaignListPanel.vue";
+import EventListPanel from "~/components/organisms/EventListPanel.vue";
+import TheaterPanel from "~/components/organisms/TheaterPanel.vue";
 export default {
-  components: { TabMenu },
+  components: {
+    TabMenu,
+    CinemaPageTitle,
+    CampaignListPanel,
+    EventListPanel,
+    TheaterPanel,
+  },
   name: "EventsPage",
 };
 </script>

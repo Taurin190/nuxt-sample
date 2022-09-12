444f46664a1b041a302f11386fa279b35db74b94
Taurin190 <taura.koichi@gmail.com>
2022-09-13 07:39:55 +0900

    add faq component

---
+++ components/organisms/FrequentQuestionsPanel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 class="mb-4">よくあるご質問</h2>
+    <v-row align="left">
+      <v-col cols="6">
+        <square-card
+          title="よくあるご質問"
+          image_path="/banner_q_a.png"
+          to="faq"
+          description="お客様からお問い合わせの多いご質問とご回答を掲載しております。お問い合わせの前にぜひ一度ご確認ください。"
+        />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import { useContext, useAsync } from "@nuxtjs/composition-api";
+import SquareCard from "../molecules/SquareCard.vue";
+export default {
+  components: { SquareCard },
+  name: "FrequentQuestionsPanel",
+  setup() {
+    const { app } = useContext();
+    const banners = useAsync(async () => {
+      const response = await app.$repositories("banner").get();
+      console.log(response);
+      return response.data;
+    });
+
+    return { banners };
+  },
+};
+</script>
--- pages/service.vue
+++ pages/service.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <tab-menu :active="5" />
-    <cinema-page-title title="サービス案内" sub_title="SERVICE INFO" />
+    <cinema-page-title
+     title="サービス案内" 
+     sub_title="SERVICE INFO" 
+     class="mb-6"
+     />
+    <frequent-questions-panel />
   </div>
 </template>
 
 <script>
 import TabMenu from "~/components/organisms/TabMenu.vue";
 import CinemaPageTitle from "~/components/atoms/CinemaPageTitle.vue";
+import FrequentQuestionsPanel from '~/components/organisms/FrequentQuestionsPanel.vue';
 export default {
-  components: { TabMenu, CinemaPageTitle },
+  components: { TabMenu, CinemaPageTitle, FrequentQuestionsPanel },
   name: "ServicePage",
 };
 </script>

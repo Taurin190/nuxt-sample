9f5b90e2e8efc09f097e64745ee452f5fc9a7503
Taurin190 <taura.koichi@gmail.com>
2022-09-18 02:58:19 +0900

    add theater panel on ticket page

--- pages/tickets.vue
+++ pages/tickets.vue
@@ -2,14 +2,22 @@
   <div>
     <tab-menu :active="6" />
     <cinema-page-title title="チケット購入・照会" sub_title="ONLINE TICKETS" />
+    <v-row>
+      <v-col cols="4">
+        <theater-panel theme_color_class="brown" />
+      </v-col>
+      <v-col cols="8">
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import TabMenu from "~/components/organisms/TabMenu.vue";
 import CinemaPageTitle from "~/components/atoms/CinemaPageTitle.vue";
+import TheaterPanel from '~/components/organisms/TheaterPanel.vue';
 export default {
-  components: { TabMenu, CinemaPageTitle },
+  components: { TabMenu, CinemaPageTitle, TheaterPanel },
   name: "TicketsPage",
 };
 </script>

49e6b5398c84db361f2c2f06992b82c2a8a317c2
Taurin190 <taura.koichi@gmail.com>
2022-10-09 02:09:34 +0900

    add image and card on ticket component

--- components/organisms/TicketSystemPanel.vue
+++ components/organisms/TicketSystemPanel.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="ticket-system-panel py-5 mb-5 px-4 rounded">
     <panel-title
-          to="#"
-          icon="mdi-ticket-confirmation"
-          title="オンラインチケット購入システム"
-          etitle="ONLINE TICKET SALES"
-          :list_link="false"
-        />
+      to="#"
+      icon="mdi-ticket-confirmation"
+      title="オンラインチケット購入システム"
+      etitle="ONLINE TICKET SALES"
+      :list_link="false"
+    />
+    <div class="px-2">
+      <v-img
+        class="my-5"
+        src="https://i.picsum.photos/id/20/600/300.jpg?hmac=h9oYqivPeqqfL8YLGiJLPwQiCqrpurvUGA0SuVwOJhE"
+      />
+    </div>
+    <explanation-card />
   </div>
 </template>
 <style scoped>
@@ -18,8 +25,9 @@ div.ticket-system-panel {
 import PanelTitle from "../molecules/PanelTitle.vue";
 import SquareCard from "../molecules/SquareCard.vue";
 import { useContext, useAsync } from "@nuxtjs/composition-api";
+import ExplanationCard from "../molecules/ExplanationCard.vue";
 export default {
-  components: { SquareCard, PanelTitle },
+  components: { SquareCard, PanelTitle, ExplanationCard },
   name: "TheaterServicePanel",
   setup() {
     const { app } = useContext();

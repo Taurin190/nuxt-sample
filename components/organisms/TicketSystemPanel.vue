ac59b8c42fd2872b4428b7a29dcc69f718f0e6c4
Taurin190 <taura.koichi@gmail.com>
2022-10-10 15:02:39 +0900

    add ticket system design

--- components/organisms/TicketSystemPanel.vue
+++ components/organisms/TicketSystemPanel.vue
@@ -13,7 +13,28 @@
         src="https://i.picsum.photos/id/20/600/300.jpg?hmac=h9oYqivPeqqfL8YLGiJLPwQiCqrpurvUGA0SuVwOJhE"
       />
     </div>
-    <explanation-card />
+    <div v-for="card in cards" :key="card.index">
+      <explanation-card :icon="card.icon" :title="card.title" />
+    </div>
+    <div class="px-2">
+      <p>※毎日AM3:00〜AM6:00は定期メンテナンス中となりますので、予めご了承ください。</p>
+    </div>
+    <div class="py-5">
+      <h3>お支払い方法</h3>
+      <ul class="py-4 pl-0">
+        <li>クレジットカード ※一括払いのみ</li>
+        <li><nuxt-link to="/">ムビチケ</nuxt-link></li>
+        <li><nuxt-link to="/">楽点ペイ（旧：楽点ID決済）</nuxt-link></li>
+      </ul>
+    </div>
+    <v-row>
+      <v-col cols="6">
+        <nuxt-link to="/">チケット購入方法 ></nuxt-link>
+      </v-col>
+      <v-col cols="6">
+        <nuxt-link to="/">よくあるご質問 ></nuxt-link>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <style scoped>
@@ -39,5 +60,26 @@ export default {
 
     return { services };
   },
+  data() {
+    return {
+      cards: [
+        {
+          icon: "mdi-run-fast",
+          title: "窓口よりも早くお求めいただけます",
+          description: "ご鑑賞希望日の3日前0時から上映直前20分前までインターネットで受け付け。"
+        },
+        {
+          icon: "mdi-sofa-single",
+          title: "全席ピンポイント座席取得",
+          description: "お好きな座席をピンポイントで取得することが可能。"
+        },
+        {
+          icon: "mdi-currency-usd-off",
+          title: "手数料無料",
+          description: "クレジットカードの決済手数料、チケット発行手数料などは一切ございませんので窓口と同じ料金でお求めいただけます。"
+        }
+      ]
+    }
+  }
 };
 </script>

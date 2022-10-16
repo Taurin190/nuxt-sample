f7dd471340f21379787d51373a7cf4e20a79c377
Taurin190 <taura.koichi@gmail.com>
2022-10-16 10:16:11 +0900

    add detail page

---
+++ pages/movies/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <tab-menu :active="2" />
+    <cinema-page-title
+      title="Sampleシネマズ 公開予定作品"
+      sub_title="COMING SOON"
+    />
+    <v-row>
+      <movie-detail-panel />
+    </v-row>
+  </div>
+</template>
+
+<script>
+import CinemaPageTitle from "~/components/atoms/CinemaPageTitle.vue";
+import CommingSoonMovieListPanel from "~/components/organisms/CommingSoonMovieListPanel.vue";
+import MovieDetailPanel from '~/components/organisms/MovieDetailPanel.vue';
+import TabMenu from "~/components/organisms/TabMenu.vue";
+import useRoute from 'vue-router';
+export default {
+  components: { TabMenu, CinemaPageTitle, CommingSoonMovieListPanel, MovieDetailPanel },
+  name: "ComingSoonPage",
+  setup() {
+    //   route = useRoute();
+    //   this.id = route.params;
+  },
+  data() {
+    return {
+        id: 0
+    }
+  }
+};
+</script>

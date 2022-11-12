4ec642bb2f10727762b283841906e0c7dc1f1c2c
Taurin190 <taura.koichi@gmail.com>
2022-11-12 16:06:15 +0900

    add v-if to wait data to load

--- pages/movies/_id.vue
+++ pages/movies/_id.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
     <tab-menu :active="3" />
-    <cinema-page-title :title="movie.title" :sub_title="movie.sub_title" />
-    <v-row>
-      <v-col cols="8">
-        <v-img :src="movie.image_path" class="mb-4" />
-        <movie-introduction-panel class="mb-4" />
-        <movie-credit-panel />
-      </v-col>
-      <v-col cols="4">
-        <road-show-card />
-        <theater-panel theme_color_class="dark_yellow" />
-      </v-col>
-    </v-row>
+    <div v-if="movie">
+      <cinema-page-title :title="movie.title" :sub_title="movie.sub_title" />
+      <v-row>
+        <v-col cols="8">
+          <v-img :src="movie.image_path" class="mb-4" />
+          <movie-introduction-panel class="mb-4" />
+          <movie-credit-panel />
+        </v-col>
+        <v-col cols="4">
+          <road-show-card />
+          <theater-panel theme_color_class="dark_yellow" />
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -31,7 +33,14 @@ import MovieCreditPanel from "~/components/organisms/MovieCreditPanel.vue";
 import MovieIntroductionPanel from "~/components/organisms/MovieIntroductionPanel.vue";
 
 export default defineComponent({
-  components: { TabMenu, CinemaPageTitle, TheaterPanel, RoadShowCard, MovieCreditPanel, MovieIntroductionPanel },
+  components: {
+    TabMenu,
+    CinemaPageTitle,
+    TheaterPanel,
+    RoadShowCard,
+    MovieCreditPanel,
+    MovieIntroductionPanel,
+  },
   name: "ComingSoonPage",
   setup() {
     const route = useRoute();
